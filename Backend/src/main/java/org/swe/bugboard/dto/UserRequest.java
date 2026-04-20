c7621b0ca8070f7d0630a260fa651e7ff0565f42
il-Drucoder <simo04pet@gmail.com>
2026-04-20 11:44:32 +0200

    Aggiunto controllo di verifica su richiesta di uno user (inserendo almeno un campo tra id, mail e username)

--- Backend/src/main/java/org/swe/bugboard/dto/UserRequest.java
+++ Backend/src/main/java/org/swe/bugboard/dto/UserRequest.java
@@ -1,5 +1,6 @@
 package org.swe.bugboard.dto;
 
+import jakarta.validation.constraints.AssertTrue;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +11,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class UserRequest {
-
-    private long id;
+    private Long id;
     private String mail;
     private String username;
     private String role;
+
+    @AssertTrue(message = "Inserire almeno un campo tra id, mail e username")
+    private boolean isValidRequest() {
+        return id != null || mail != null || username != null;
+    }
 }
