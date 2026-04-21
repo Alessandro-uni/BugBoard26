caf8731ff222ef1554f625af09a3df6eff1870e4
il-Drucoder <simo04pet@gmail.com>
2026-04-22 00:37:43 +0200

    Inizio creazione di UserController

--- Backend/src/main/java/org/swe/bugboard/controller/UserController.java
+++ Backend/src/main/java/org/swe/bugboard/controller/UserController.java
@@ -1,4 +1,28 @@
 package org.swe.bugboard.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.swe.bugboard.dto.UserResponse;
+import org.swe.bugboard.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> getCurrentUser(Authentication authentication) {
+        String userEmail = authentication.getName();
+        return ResponseEntity.ok(userService.getUserByMail(userEmail));
+    }
+
+//    @GetMapping("/issues/{id}")
+//    public ResponseEntity<>
+//
+//    @PutMapping("/changepw")
+//    public ResponseEntity<UserResponse> changePassword(@RequestBody )
+
 }
