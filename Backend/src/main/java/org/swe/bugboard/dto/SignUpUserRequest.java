62ab0115e6d67cb07a6dc3469fc7c4afe9a00b4f
il-Drucoder <simo04pet@gmail.com>
2026-04-22 00:36:54 +0200

    Rimossi import non utilizzati

--- Backend/src/main/java/org/swe/bugboard/dto/SignUpUserRequest.java
+++ Backend/src/main/java/org/swe/bugboard/dto/SignUpUserRequest.java
@@ -1,13 +1,10 @@
 package org.swe.bugboard.dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.management.relation.Role;
 
 @Data
 @NoArgsConstructor
