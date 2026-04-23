261c29a1173d44c8775fb95d7c20eb1aa390b7b0
il-Drucoder <simo04pet@gmail.com>
2026-04-23 13:06:38 +0200

    Creazione dto che si occupa di gestire la richiesta di cambio password

---
+++ Backend/src/main/java/org/swe/bugboard/dto/ChangePasswordUserRequest.java
@@ -0,0 +1,32 @@
+package org.swe.bugboard.dto;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChangePasswordUserRequest {
+    @NotEmpty(message = "Password corrente assente")
+    private CharSequence oldRawPassword;
+
+    @NotEmpty(message = "Password nuova assente")
+    private CharSequence newRawPassword;
+
+    @NotEmpty(message = "Password nuova ripetuta assente")
+    private CharSequence repeatNewRawPassword;
+
+    @AssertTrue(message = "La nuova password e la conferma non coincido")
+    public boolean isRepeatPasswordMatch() {
+        if (newRawPassword != null && repeatNewRawPassword != null) {
+            return newRawPassword.toString().equals(repeatNewRawPassword.toString());
+        }
+
+        return true;
+    }
+}
