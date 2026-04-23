efb9cdf059c3b031adf8600624ab6c39f0b56850
il-Drucoder <simo04pet@gmail.com>
2026-04-23 13:06:05 +0200

    Aggiunto attributo di password ripetuta con verifica che sia uguale alla password effettiva

--- Backend/src/main/java/org/swe/bugboard/dto/SignUpUserRequest.java
+++ Backend/src/main/java/org/swe/bugboard/dto/SignUpUserRequest.java
@@ -1,5 +1,6 @@
 package org.swe.bugboard.dto;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,9 +18,21 @@ public class SignUpUserRequest {
     @NotEmpty(message = "Password assente")
     private CharSequence rawPassword;
 
+    @NotEmpty(message = "Password ripetuta assente")
+    private CharSequence repeatRawPassword;
+
     @NotEmpty(message = "Username assente")
     private String username;
 
     @NotEmpty(message = "Ruolo assente")
     private String role;
+
+    @AssertTrue(message = "La nuova password e la conferma non coincido")
+    public boolean isRepeatPasswordMatch() {
+        if (rawPassword != null && repeatRawPassword != null) {
+            return rawPassword.toString().equals(repeatRawPassword.toString());
+        }
+
+        return true;
+    }
 }
