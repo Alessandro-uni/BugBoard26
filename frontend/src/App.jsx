01024eea2ac516815b1e2d4638a7514d6890f623
alessya97 <alessia.picari4@gmail.com>
2026-04-23 17:34:29 +0200

    Prima connessione frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,135 @@
+import { useState } from 'react'
+import LoginPage from './components/LoginPage.jsx';
+//import './App.css'
+
+function App() {
+
+  /*
+    useState resituisce una array che contiene due elementi [il primo = stato attuale / il secondo = stato aggiornato]
+   */
+
+  //creo una costante per verificare lo stato: l'utente è loggato? false = no, true = si
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //verifico quale pagina è attualmente visibile nell'app
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const [selectIssueId, setSelectIssueId] = useState(null);
+
+  // modalità aggiungi utente
+  const [isAddUserModalOpen, setIsAddUserModalOpen] = useState(false);
+
+  // modalità cambia pw
+  const [isChangePwModalOpen, setIsChangePwModalOpen] = useState(false);
+
+  /*
+    FUNZIONI PER LA GESTIONE DI EVENTI = reagisce a un'azione dell'utente e decide cosa deve succedere nell'app
+    si occupano di cambiare stato
+   */
+
+  //funzione per l'accesso del login: accetta due parametri username e pw
+  const handleLogin = (username, password) => {
+
+    //verifica che non siano entrambi  vuoti
+    if(username && password){
+      setIsLoggedIn(true); //segna l'utente come autenticato tramite useState che aggiorna lo stato
+    }
+    setUserInfo(username);
+
+  };
+
+  //funzione che aggiorna le pagine
+  const handleNavigation = (page) => {
+
+    //controlli
+    if(page == 'Logout') {
+      setIsLoggedIn(false);
+      setCurrentPage('Home');
+      setSelectIssueId(null);
+    }else if (page == 'Aggiungi User') {
+      setIsAddUserModalOpen(true);
+    }else if (page == 'Cambia Password') {
+      setIsChangePwModalOpen(true);
+    }else {
+      setCurrentPage(page);
+      setSelectIssueId(null);
+    }
+  };
+
+  //funziona che porta l'utente dalla  pagina generale  ViewIssue a quella specifica di un singolo issue
+  const handleViewIssue = (issueId) => {
+    setSelectIssueId(issueId); //indichi quale issue hai selezionato
+    setCurrentPage('Visualizza Singola Issue'); //aggiorna la pagina
+  };
+
+  //funzione di torna indietro
+  const handleBackToList = () => {
+    setSelectIssueId(null);
+    setCurrentPage('Visualizza tutte le Issues');
+  };
+
+  //CONTROLLO = se l'utente non accede, mostra solo la pagina di login
+  if(!isLoggedIn) {
+    return <LoginPage onLogin={handleLogin}/>;
+  }
+
+  /*
+    FUNZIONE = definiamo cosa deve apparire fisicamente sullo schermo
+   */
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <HomePage onViewIssue={handleViewIssue} />;
+
+      case 'Visualizza tutte le Issues':
+        return <ViewAllIssuesPage onViewIssue={handleViewIssue} />;
+
+      case 'Crea Issue':
+        return <CreateIssuePage />;
+
+      case 'Assegna Issues':
+        return <AssignIssuesPage />;
+
+      case 'Visulizza singola Issue':
+        return selectIssueId ? (
+            <ViewSingleIssuePage issueId={selectIssueId} onBack={handleBackToList()}/>
+        ):(
+            <HomePage onViewIssue={handleViewIssue} />
+        );
+
+      default:
+        return <HomePage onViewIssue={handleViewIssue} />;
+    }
+  };
+
+
+  //struttura visiva principale dell'app
+  return (
+    <div className="min-h-screen w-full flex flex-col md:flex-row bg-gray-50">
+        <Menu currentPage={currentPage} onNavigate={handleNavigation}/>
+
+        <div className="flex-1 flex flex-col min-w-0">
+          <Header />
+          <main className="p-4 md:p-8 lg:p-12 overflow-y-auto">
+            <div className="max-w-7xl mx-auto w-full">
+              {renderPage()}
+            </div>
+          </main>
+        </div>
+
+
+          <AddUserModal
+            isOpen={isAddUserModalOpen}
+            onClose={() => setIsAddUserModalOpen(false)}
+          />
+
+          <ChangePwModal
+            isOpen={isChangePwModalOpen}
+            onClone={() => setIsChangePwModalOpen(false)}
+          />
+        
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,90 @@
+import {useState} from "react";
+//import {User} from 'lucide-react';
+
+function LoginPage({onLogin}) {
+
+    //creiamo le variabili
+    const [username,setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    //funzione: quando premi il tasto accedi si crea un oggetto che contiene tutte le info su quel click
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:8080/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json',
+                },
+                body: JSON.stringify({username: username, password: password }),
+            });
+
+            if(response.ok){
+                const userData = await response.json();
+                console.log("Login riuscito:", userData);
+
+                onLogin(userData);
+            }else{
+                const errorText = await response.text();
+                alert("Errore: " + errorText);
+            }
+        } catch (error){
+            console.error("Errore di connessione:", error);
+            alert("il server non risponde");
+        }
+
+
+    };
+
+    return (
+        <div className="size-full flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100">
+
+            <div className="w-full max-w-md p-8 bg-white rounded-2xl shadow-xl">
+                <div className="text-center mb-8">
+
+                        <h1 className="text-3xl font-bold text-gray-900 mb-2"> Benvenuto</h1>
+                        <p className="text-gray-600"> Accedi al tuo account</p>
+                </div>
+
+                <form onSubmit={handleSubmit} className="space-y-6">
+                    {/*inserimeto username*/}
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 mb-2">Mail</label>
+                        <div className="relative">
+                            <div>
+
+                            </div>
+                            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}
+                                className="block w-full pl-10 pr-3 py-3 border border-gray-300 rounded-lg"
+                                   placeholder="Inserisci la tua mail"
+                                   required
+                            />
+                        </div>
+                    </div>
+
+                    {/*inserimento pw*/}
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 mb-2">Password</label>
+                        <div className="relative">
+                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center">
+
+                            </div>
+                            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}
+                                   className="block w-full pl-10 pr-3 py-3 border border-gray-300 rounded-lg"
+                                   placeholder="Inserisci la tua password"
+                                   required
+                            />
+                        </div>
+                    </div>
+
+                    <button type="submit" className="w-full py-3 px-4 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg">Accedi</button>
+
+                </form>
+            </div>
+        </div>
+    );
+
+}
+
+export default LoginPage;
