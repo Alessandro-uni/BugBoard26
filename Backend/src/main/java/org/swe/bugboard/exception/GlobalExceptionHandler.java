acf15013f1ca12bb7210fd56be92bb9ad611fbab
il-Drucoder <simo04pet@gmail.com>
2026-04-28 12:29:10 +0200

    Aggiunto file di gestione eccezioni

---
+++ Backend/src/main/java/org/swe/bugboard/exception/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package org.swe.bugboard.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Map<String, Object>> handleAccessDenied(AccessDeniedException e) {
+        return buildErrorResponse(HttpStatus.FORBIDDEN, "Accesso negato", e.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleEntityNotFound(EntityNotFoundException e) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, "Risorsa non trovata", e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationError(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error: e.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "Errore di validazione", errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleAllOtherException(Exception e) {
+        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Errore interno", e.getMessage());
+    }
+
+    private ResponseEntity<Map<String, Object>> buildErrorResponse(HttpStatus httpStatus, String error, Object message) {
+        Map<String, Object> bodyResponse = new HashMap<>();
+        bodyResponse.put("timestamp", LocalDateTime.now());
+        bodyResponse.put("status", httpStatus.value());
+        bodyResponse.put("error", error);
+        bodyResponse.put("message", message);
+
+        return ResponseEntity.status(httpStatus).body(bodyResponse);
+    }
+}
