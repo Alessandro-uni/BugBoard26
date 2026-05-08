6a81cdf1fb5e6b4bd30acf61ca59cc3538171441
il-Drucoder <simo04pet@gmail.com>
2026-05-08 20:52:02 +0200

    Aggiunto CrossOrigin mancante

--- Backend/src/main/java/org/swe/bugboard/controller/TagController.java
+++ Backend/src/main/java/org/swe/bugboard/controller/TagController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/tags")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class TagController {
     private final TagService tagService;
 
