08fd5b85cd570310ca244d18b3df42fcb550ab9f
il-Drucoder <simo04pet@gmail.com>
2026-05-08 18:04:45 +0200

    Aggiunto promemoria per quando verrà aggiunto il db persistente

--- Backend/src/main/java/org/swe/bugboard/security/SecurityConfig.java
+++ Backend/src/main/java/org/swe/bugboard/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/api/**").permitAll() // todo: dopo aver collegato il db persistente mettere /auth/login
                         .anyRequest().authenticated()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
