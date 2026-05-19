01c5ec4158cb1df64f5bf8b6b92921569bd65bfe
il-Drucoder <simo04pet@gmail.com>
2026-05-19 12:23:01 +0200

    Aggiunta logica per verificare i permessi dei ruoli

--- Backend/src/main/java/org/swe/bugboard/model/UserRole.java
+++ Backend/src/main/java/org/swe/bugboard/model/UserRole.java
@@ -1,7 +1,23 @@
 package org.swe.bugboard.model;
 
 public enum UserRole {
-    USER,
-    ADMIN,
-    LURKER
+    USER(true, true),
+    ADMIN(true, true),
+    LURKER(false, false);
+
+    private final boolean canReportIssue;
+    private final boolean canBeAssignedToIssue;
+
+    UserRole(boolean canReportIssue, boolean canBeAssignedToIssue) {
+        this.canReportIssue = canReportIssue;
+        this.canBeAssignedToIssue = canBeAssignedToIssue;
+    }
+
+    public boolean canReportIssue() {
+        return canReportIssue;
+    }
+
+    public boolean canBeAssignedToIssue() {
+        return canBeAssignedToIssue;
+    }
 }
