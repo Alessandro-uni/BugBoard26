e60ba5373d3cff8184eefc0b253baf761d377bab
il-Drucoder <simo04pet@gmail.com>
2026-05-19 12:29:15 +0200

    Aggiunta logica per verificare le proprietà degli stati delle issue

--- Backend/src/main/java/org/swe/bugboard/model/IssueStatus.java
+++ Backend/src/main/java/org/swe/bugboard/model/IssueStatus.java
@@ -1,9 +1,24 @@
 package org.swe.bugboard.model;
 
-
 public enum IssueStatus {
-    TODO,
-    INPROGRESS,
-    RESOLVED,
-    CLOSED
+    TODO(true, true),
+    INPROGRESS(true, true),
+    RESOLVED(false, true),
+    CLOSED(false, false);
+
+    private final boolean isWorkload;
+    private final boolean isModifiable;
+
+    IssueStatus(boolean isWorkload, boolean isModifiable) {
+        this.isWorkload = isWorkload;
+        this.isModifiable = isModifiable;
+    }
+
+    public boolean isWorkload() {
+        return isWorkload;
+    }
+
+    public boolean isModifiable() {
+        return isModifiable;
+    }
 }
