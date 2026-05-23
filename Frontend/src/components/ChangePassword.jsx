430e1231ff0b0a58d214edb31e87cd7216f22d0c
il-Drucoder <simo04pet@gmail.com>
2026-05-23 20:01:39 +0200

    Corretto il funzionamento di cambio password

--- Frontend/src/components/ChangePassword.jsx
+++ Frontend/src/components/ChangePassword.jsx
@@ -2,61 +2,54 @@ import {useState} from "react";
 //import {User} from 'lucide-react';
 
 function ChangePassword({onChangePassword}) {
-
-    //creiamo le variabili
-
-    const [rawPassword, setRawPassword] = useState('');
-    const [repeatRawPassword, setRepeatRawPassword] = useState('');
+    const [oldRawPassword, setOldRawPassword] = useState('');
+    const [newRawPassword, setNewRawPassword] = useState('');
+    const [repeatNewRawPassword, setRepeatNewRawPassword] = useState('');
 
 
-    //funzione: quando premi il tasto accedi si crea un oggetto che contiene tutte le info su quel click
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        const token = localStorage.getItem('token');
+
         try {
-            const response = await fetch('http://localhost:8080/api/auth/login', {
-                method: 'POST',
+            const response = await fetch('http://localhost:8080/api/users/me/password', {
+                method: 'PUT',
                 headers: {
                     'Content-Type' : 'application/json',
+                    'Authorization': `Bearer ${token}`
                 },
-                body: JSON.stringify({ rawPassword: rawPassword, repeatRawPassword: repeatRawPassword }),
+                body: JSON.stringify({oldRawPassword: oldRawPassword, newRawPassword: newRawPassword, repeatNewRawPassword: repeatNewRawPassword}),
             });
 
-            if(response.ok){
+            if (response.ok) {
                 const userData = await response.json();
-                console.log("Creazione riuscita:", userData);
-
                 onChangePassword(userData);
-            }else{
+            } else {
                 const errorJson = await response.json();
-                alert("Errore: " + errorJson.message);
+                alert("Errore: " + errorJson.message.oldRawPassword + errorJson.message.newRawPassword);
             }
+
         } catch (error){
-            console.error("Errore di connessione:", error);
-            alert("il server non risponde");
+            console.error("Errore nella chiamata al backend:", error);
+            alert('Errore: ' + error.message);
         }
-
-
     };
 
     return (
-        <div className="size-full flex items-center justify-center bg-gradient-to-br ">
+        <div className="size-full flex items-center justify-center bg-linear-to-br ">
 
             <div className="w-full max-w-md p-8 bg-white rounded-2xl shadow-xl">
                 <div className="text-center mb-8">
-
                     <p className="text-gray-600"> Crea una nuova password</p>
                 </div>
 
                 <form onSubmit={handleSubmit} className="space-y-6">
-                    {/*inserimeto vecchia pw*/}
+                    {/* Inserimeto vecchia pw */}
                     <div>
-                        <label className="block text-sm font-medium text-gray-700 mb-2">Vecchia password</label>
+                        <label className="block text-sm font-medium text-gray-700 mb-2">Password attuale</label>
                         <div className="relative">
-                            <div>
-
-                            </div>
-                            <input type="text" value={rawPassword} onChange={(e) => setRawPassword(e.target.value)}
+                            <input type="password" value={oldRawPassword} onChange={(e) => setOldRawPassword(e.target.value)}
                                    className="block w-full pl-10 pr-3 py-3 border border-gray-300 rounded-lg"
                                    placeholder="Inserisci la vecchia password"
                                    required
@@ -64,14 +57,11 @@ function ChangePassword({onChangePassword}) {
                         </div>
                     </div>
 
-                    {/*inserimento pw*/}
+                    {/* Inserimento nuova pw*/}
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-2">Nuova password</label>
                         <div className="relative">
-                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center">
-
-                            </div>
-                            <input type="password" value={rawPassword} onChange={(e) => setRawPassword(e.target.value)}
+                            <input type="password" value={newRawPassword} onChange={(e) => setNewRawPassword(e.target.value)}
                                    className="block w-full pl-10 pr-3 py-3 border border-gray-300 rounded-lg"
                                    placeholder="Inserisci la nuova password"
                                    required
@@ -79,23 +69,17 @@ function ChangePassword({onChangePassword}) {
                         </div>
                     </div>
 
-                    {/*inserimento ripeti pw*/}
+                    {/* Inserimento ripeti nuova pw*/}
                     <div>
-                        <label className="block text-sm font-medium text-gray-700 mb-2">Ripeti password</label>
+                        <label className="block text-sm font-medium text-gray-700 mb-2">Ripeti nuova password</label>
                         <div className="relative">
-                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center">
-
-                            </div>
-                            <input type="password" value={repeatRawPassword} onChange={(e) => setRepeatRawPassword(e.target.value)}
+                            <input type="password" value={repeatNewRawPassword} onChange={(e) => setRepeatNewRawPassword(e.target.value)}
                                    className="block w-full pl-10 pr-3 py-3 border border-gray-300 rounded-lg"
-                                   placeholder="Inserisci la nuova  password"
+                                   placeholder="Conferma la nuova  password"
                                    required
                             />
                         </div>
                     </div>
-
-
-
 
                     <button type="submit" className="w-full py-3 px-4 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg">Cambia password</button>
 
@@ -103,7 +87,6 @@ function ChangePassword({onChangePassword}) {
             </div>
         </div>
     );
-
 }
 
 export default ChangePassword;
