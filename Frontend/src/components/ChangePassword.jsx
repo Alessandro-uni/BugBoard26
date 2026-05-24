c130e84e14f7a908bcb9400036f86516586b0101
il-Drucoder <simo04pet@gmail.com>
2026-05-25 00:13:10 +0200

    Aggiunti pulsanti "occhio" per visualizzare in chiaro la password che si sta inserendo

--- Frontend/src/components/ChangePassword.jsx
+++ Frontend/src/components/ChangePassword.jsx
@@ -1,12 +1,19 @@
-import {useState} from "react";
-//import {User} from 'lucide-react';
+import React, {useState} from "react";
+import {Eye, EyeOff} from "lucide-react";
 
 function ChangePassword({onChangePassword}) {
-    const [oldRawPassword, setOldRawPassword] = useState('');
+    const [currentRawPassword, setCurrentRawPassword] = useState('');
     const [newRawPassword, setNewRawPassword] = useState('');
     const [repeatNewRawPassword, setRepeatNewRawPassword] = useState('');
 
+    const [showCurrentPassword, setShowCurrentPassword] = useState(false);
+    const [showNewPassword, setShowNewPassword] = useState(false);
+    const [showRepeatNewPassword, setShowRepeatNewPassword] = useState(false);
+    const changeCurrentPasswordVisibility = () => setShowCurrentPassword(!showCurrentPassword);
+    const changeNewPasswordVisibility = () => setShowNewPassword(!showNewPassword);
+    const changeRepeatNewPasswordVisibility = () => setShowRepeatNewPassword(!showRepeatNewPassword);
 
+    // Gestione invio modulo
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -19,7 +26,7 @@ function ChangePassword({onChangePassword}) {
                     'Content-Type' : 'application/json',
                     'Authorization': `Bearer ${token}`
                 },
-                body: JSON.stringify({oldRawPassword: oldRawPassword, newRawPassword: newRawPassword, repeatNewRawPassword: repeatNewRawPassword}),
+                body: JSON.stringify({currentRawPassword: currentRawPassword, newRawPassword: newRawPassword, repeatNewRawPassword: repeatNewRawPassword}),
             });
 
             if (response.ok) {
@@ -27,7 +34,7 @@ function ChangePassword({onChangePassword}) {
                 onChangePassword(userData);
             } else {
                 const errorJson = await response.json();
-                alert("Errore: " + errorJson.message.oldRawPassword + errorJson.message.newRawPassword);
+                alert("Errore: " + errorJson.message.currentRawPassword + errorJson.message.newRawPassword);
             }
 
         } catch (error){
@@ -37,23 +44,34 @@ function ChangePassword({onChangePassword}) {
     };
 
     return (
-        <div className="size-full flex items-center justify-center bg-linear-to-br ">
 
+        <div className="p-6">
+        <div className="max-w-md mx-auto">
+            <h3 className="text-lg font-semibold text-gray-900 mb-4">Cambia password</h3>
+            {/* Card del Form */}
             <div className="w-full max-w-md p-8 bg-white rounded-2xl shadow-xl">
-                <div className="text-center mb-8">
-                    <p className="text-gray-600"> Crea una nuova password</p>
-                </div>
-
                 <form onSubmit={handleSubmit} className="space-y-6">
-                    {/* Inserimeto vecchia pw */}
+
+                    {/* Inserimeto pw corrente */}
                     <div>
-                        <label className="block text-sm font-medium text-gray-700 mb-2">Password attuale</label>
+                        <label className="block text-sm font-medium text-gray-700 mb-2">Password corrente</label>
                         <div className="relative">
-                            <input type="password" value={oldRawPassword} onChange={(e) => setOldRawPassword(e.target.value)}
+                            <input type={showCurrentPassword ? "text" : "password"}
+                                   value={currentRawPassword}
+                                   onChange={(e) => setCurrentRawPassword(e.target.value)}
                                    className="block w-full pl-10 pr-3 py-3 border border-gray-300 rounded-lg"
-                                   placeholder="Inserisci la vecchia password"
+                                   placeholder="Inserisci la tua password corrente"
                                    required
                             />
+
+                            {/* Pulsante occhio */}
+                            <button
+                                type="button"
+                                onClick={changeCurrentPasswordVisibility}
+                                className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 hover:text-gray-600 focus:outline-none"
+                            >
+                                {showCurrentPassword ? <Eye size={20}/> : <EyeOff size={20}/>}
+                            </button>
                         </div>
                     </div>
 
@@ -61,11 +79,22 @@ function ChangePassword({onChangePassword}) {
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-2">Nuova password</label>
                         <div className="relative">
-                            <input type="password" value={newRawPassword} onChange={(e) => setNewRawPassword(e.target.value)}
+                            <input type={showNewPassword ? "text" : "password"}
+                                   value={newRawPassword}
+                                   onChange={(e) => setNewRawPassword(e.target.value)}
                                    className="block w-full pl-10 pr-3 py-3 border border-gray-300 rounded-lg"
                                    placeholder="Inserisci la nuova password"
                                    required
                             />
+
+                            {/* Pulsante occhio */}
+                            <button
+                                type="button"
+                                onClick={changeNewPasswordVisibility}
+                                className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 hover:text-gray-600 focus:outline-none"
+                            >
+                                {showNewPassword ? <Eye size={20}/> : <EyeOff size={20}/>}
+                            </button>
                         </div>
                     </div>
 
@@ -73,18 +102,33 @@ function ChangePassword({onChangePassword}) {
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-2">Ripeti nuova password</label>
                         <div className="relative">
-                            <input type="password" value={repeatNewRawPassword} onChange={(e) => setRepeatNewRawPassword(e.target.value)}
+                            <input type={showRepeatNewPassword ? "text" : "password"}
+                                   value={repeatNewRawPassword}
+                                   onChange={(e) => setRepeatNewRawPassword(e.target.value)}
                                    className="block w-full pl-10 pr-3 py-3 border border-gray-300 rounded-lg"
-                                   placeholder="Conferma la nuova  password"
+                                   placeholder="Conferma la nuova password"
                                    required
                             />
+
+                            {/* Pulsante occhio */}
+                            <button
+                                type="button"
+                                onClick={changeRepeatNewPasswordVisibility}
+                                className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 hover:text-gray-600 focus:outline-none"
+                            >
+                                {showRepeatNewPassword ? <Eye size={20}/> : <EyeOff size={20}/>}
+                            </button>
                         </div>
                     </div>
 
-                    <button type="submit" className="w-full py-3 px-4 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg">Cambia password</button>
+                    {/* Pulsante di conferma */}
+                    <button type="submit" className="w-full py-3 px-4 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg">
+                        Modifica
+                    </button>
 
                 </form>
             </div>
+        </div>
         </div>
     );
 }
