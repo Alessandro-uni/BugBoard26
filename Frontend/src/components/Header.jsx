3b95efedc670245e9bd0c347f5e441033365e714
il-Drucoder <simo04pet@gmail.com>
2026-05-26 21:04:43 +0200

    Rimosse icona e prop non utilizzati

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,30 +1,24 @@
 import React from "react";
-import {Home, Menu , X } from 'lucide-react';
+import {Home, Menu} from 'lucide-react';
 
-
-function Header ({isMenuOpen, onToggleMenu, onHomeClick}){
+function Header ({onToggleMenu, onHomeClick}){
     return (
         <header className="h-16 bg-white border-b border-gray-200 flex items-center justify-between px-6" >
-
 
             {/*hamburger menu*/}
             <button
                 onClick={onToggleMenu}
                 className="p-2 hover:bg-gray-100 rounded-lg transition-colors"
-                aria-label="Toggle menu"
             >
                 <Menu size={24} className="text-gray-900" />
             </button>
 
-
             <button
                 onClick={onHomeClick}
                 className="p-2 hover:bg-gray-100 rounded-lg transition-colors"
-                aria-label="Go to home"
             >
                 <Home size={24} className="text-gray-900" />
             </button>
-
 
         </header>
     );
