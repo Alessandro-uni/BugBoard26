42deb41ed0b5d480ce6de404ba98e09141922fef
il-Drucoder <simo04pet@gmail.com>
2026-05-27 16:21:12 +0200

    Unificazione pop up

--- Frontend/src/components/ViewIssueList.jsx
+++ Frontend/src/components/ViewIssueList.jsx
@@ -1,17 +1,15 @@
 import React, {useEffect, useState} from "react";
-import { Filter, ArrowUpDown, Download, Frown } from 'lucide-react';
+import {SlidersHorizontal, Download, Frown} from 'lucide-react';
 
-import SortPopUp from './SortPopUp';
-import FilterPopUp from './FilterPopUp';
+import FilterAndSortPopUp from './FilterAndSortPopUp.jsx';
 import ExportPopUp from './ExportPopUp';
 import {Badge} from "./Badge.jsx";
 import {Button} from "./Button.jsx";
 
 function ViewIssueList({onViewIssue, bodyParams, pageName}) {
     // Stati per i popup
-    const [isSortPopUpOpen, setIsSortPopUpOpen] = useState(false);
+    const [isFilterAndSortPopUpOpen, setIsFilterAndSortPopUpOpen] = useState(false);
     const [isExportPopUpOpen, setIsExportPopUpOpen] = useState(false);
-    const [isFilterPopUpOpen, setIsFilterPopUpOpen] = useState(false);
 
     // Stati per i dati e la paginazione
     const [filteredIssues, setFilteredIssues] = useState([]);
@@ -84,23 +82,15 @@ function ViewIssueList({onViewIssue, bodyParams, pageName}) {
                 </div>
 
                 {/* Barra degli strumenti (Buttons : Filtri/Ordina/Esporta) */}
-                <div className="p-4 border-b border-gray-200">
+                <div className="px-4 pb-4 border-gray-200">
                     <div className="flex flex-col md:flex-row items-start md:items-center gap-4">
 
                         <Button
-                            onClick={() => setIsFilterPopUpOpen(true)}
+                            onClick={() => setIsFilterAndSortPopUpOpen(true)}
                             className="flex items-center gap-2 px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors"
                         >
-                            <Filter size={20} />
-                            Filtra
-                        </Button>
-
-                        <Button
-                            onClick={() => setIsSortPopUpOpen(true)}
-                            className="flex items-center gap-2 px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors"
-                        >
-                            <ArrowUpDown size={20} />
-                            Ordina
+                            <SlidersHorizontal size={20} />
+                            Filtra e Ordina
                         </Button>
 
                         <Button
@@ -115,25 +105,25 @@ function ViewIssueList({onViewIssue, bodyParams, pageName}) {
                 </div>
 
                 {/* Contenitore Tabella */}
-                <div className="bg-white rounded-xl shadow-sm">
+                <div className="bg-white rounded-xl shadow-sm p-5">
                     {/* Elenco Issue */}
                     {filteredIssues.length === 0 ? (
-                        <div className="flex flex-col items-center justify-center p-12 text-center bg-gray-50/50 rounded-xl border-2 border-dashed border-gray-200 m-4">
+                        <div className="flex flex-col items-center justify-center p-10 text-center bg-gray-50/50 rounded-xl border-2 border-dashed border-gray-200">
                             <div className="bg-white p-3 rounded-full shadow-sm mb-4">
                                 <Frown size={32} className="text-gray-400"/>
                             </div>
                             <h3 className="text-lg font-medium text-gray-900 mb-1">Nessuna issue trovata</h3>
                             <p className="text-gray-500 max-w-sm">
-                                Non ci sono risultati in questa sezione. Provare a modificare i filtri.
+                                Non ci sono risultati in questa sezione. Provare a modificare i filtri
                             </p>
                         </div>
                     ) : (
-                        <div className="flex flex-col">
+                        <div className="flex flex-col bg-gray-50/50 rounded-xl border-2 border-dashed border-gray-200">
                             {filteredIssues.map((issue) => (
                                 <div
                                     key={issue.id}
                                     onClick={() => onViewIssue(issue.id)}
-                                    className="group p-5 border border-transparent border-gray-100 hover:bg-gray-200 hover:shadow-md hover:translate-y-0.5 bg-white transition-all duration-200 cursor-pointer"
+                                    className="group p-5 border border-transparent rounded-xl border-gray-100 hover:bg-gray-200 hover:shadow-md hover:translate-y-0.5 bg-white transition-all duration-200 cursor-pointer"
                                 >
                                     <div className="flex items-start justify-between">
                                         <div className="flex items-start gap-3 flex-1">
@@ -162,8 +152,10 @@ function ViewIssueList({onViewIssue, bodyParams, pageName}) {
 
                     {/* Controlli paginazione */}
                     {filteredIssues.length > 0 && (
-                        <div className="flex items-center justify-between px-4 py-3 bg-gray-50 border-t border-gray-200 sm:px-6 rounded-b-xl">
+                        <div className="flex items-center justify-between px-4 py-3 rounded-xl bg-gray-50 border-gray-200 sm:px-6 rounded-b-xl">
+                            {/* Pulsanti per schermo piccolo (smartphone) */}
                             <div className="flex justify-between flex-1 sm:hidden">
+
                                 <Button
                                     onClick={handlePrevPage}
                                     disabled={currentPage === 1}
@@ -171,6 +163,11 @@ function ViewIssueList({onViewIssue, bodyParams, pageName}) {
                                 >
                                     Precedente
                                 </Button>
+                                <div>
+                                    <p className="text-sm p-2 text-gray-700">
+                                        Pagina <span className="font-medium">{currentPage}</span> di <span className="font-medium">{totalPages}</span>
+                                    </p>
+                                </div>
                                 <Button
                                     onClick={handleNextPage}
                                     disabled={currentPage === totalPages}
@@ -180,6 +177,7 @@ function ViewIssueList({onViewIssue, bodyParams, pageName}) {
                                 </Button>
                             </div>
 
+                            {/* Pulsanti per schermo grande (computer) */}
                             <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
                                 <div>
                                     <p className="text-sm text-gray-700">
@@ -188,11 +186,11 @@ function ViewIssueList({onViewIssue, bodyParams, pageName}) {
                                 </div>
 
                                 <div>
-                                    <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px">
+                                    <nav className="relative z-0 inline-flex rounded-md space-x-2">
                                         <Button
                                             onClick={handlePrevPage}
                                             disabled={currentPage === 1}
-                                            className="relative inline-flex items-center px-3 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
+                                            className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
                                         >
                                             Precedente
                                         </Button>
@@ -211,9 +209,8 @@ function ViewIssueList({onViewIssue, bodyParams, pageName}) {
                 </div>
             </div>
 
-            <SortPopUp isOpen={isSortPopUpOpen} onClose={() => setIsSortPopUpOpen(false)} />
-            <ExportPopUp isOpen={isExportPopUpOpen} onClose={() => setIsExportPopUpOpen(false)} />
-            <FilterPopUp isOpen={isFilterPopUpOpen} onClose={() => setIsFilterPopUpOpen(false)} />
+            <FilterAndSortPopUp isOpen={isFilterAndSortPopUpOpen} onClose={() => setIsFilterAndSortPopUpOpen(false)}/>
+            <ExportPopUp isOpen={isExportPopUpOpen} onClose={() => setIsExportPopUpOpen(false)}/>
         </div>
     );
 }
