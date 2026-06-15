c86f995fc22b2e40adbe4acfbc232ec627fa6b30
il-Drucoder <simo04pet@gmail.com>
2026-06-15 22:08:01 +0200

    Creato/Utilizzato box di caricamento

--- Frontend/src/components/HomePage.jsx
+++ Frontend/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {IssueCard} from "./IssueCard.jsx";
+import {ReloadingBox} from "./ReloadingBox.jsx";
 
 function IssueSection({title, issues, onViewIssue, onViewAll}) {
     return (
@@ -40,6 +41,7 @@ function HomePage({onViewIssue, currentUserId, userName, userRole = 'LURKER', on
     const [allIssues, setAllIssues] = useState([]);
     const [assignedIssues, setAssignedIssues] = useState([]);
     const [reportedIssues, setReportedIssues] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const MAX_HOME_ISSUES = 3;
 
@@ -74,6 +76,8 @@ function HomePage({onViewIssue, currentUserId, userName, userRole = 'LURKER', on
         };
 
         const fetchAllData = async () => {
+            setIsLoading(true);
+
             try {
                 const [allData, assignedData, reportedData] = await Promise.all([
                     fetchIssueGroup({pageNumber: 0, pageSize: MAX_HOME_ISSUES}),
@@ -87,6 +91,8 @@ function HomePage({onViewIssue, currentUserId, userName, userRole = 'LURKER', on
             } catch (error) {
                 console.error("Errore nella chiamata al backend:", error);
                 alert('Errore: ' + error.message);
+            } finally {
+                setIsLoading(false);
             }
         };
 
@@ -102,31 +108,37 @@ function HomePage({onViewIssue, currentUserId, userName, userRole = 'LURKER', on
                 <p className="text-gray-600 dark:text-gray-400">Ciao, {userName}. Benvenutə nella tua area di lavoro</p>
             </div>
 
-            <div className="space-y-6 mx-auto">
-                <IssueSection
-                    title="Tutte le issue"
-                    issues={allIssues}
-                    onViewIssue={onViewIssue}
-                    onViewAll={() => onNavigation('Tutte le issue')}
-                />
+            {isLoading ? (
+                <ReloadingBox description='Caricamento issue in corso...'></ReloadingBox>
+            ) : (
+                <div>
+                    <div className="space-y-6 mx-auto">
+                        <IssueSection
+                            title="Tutte le issue"
+                            issues={allIssues}
+                            onViewIssue={onViewIssue}
+                            onViewAll={() => onNavigation('Tutte le issue')}
+                        />
 
-                {['USER', 'ADMIN'].includes(userRole) && (
-                    <>
-                        <IssueSection
-                            title="Issue assegnate"
-                            issues={assignedIssues}
-                            onViewIssue={onViewIssue}
-                            onViewAll={() => onNavigation('Issue assegnate')}
-                        />
-                        <IssueSection
-                            title="Issue segnalate"
-                            issues={reportedIssues}
-                            onViewIssue={onViewIssue}
-                            onViewAll={() => onNavigation('Issue segnalate')}
-                        />
-                    </>
-                )}
-            </div>
+                        {['USER', 'ADMIN'].includes(userRole) && (
+                            <>
+                                <IssueSection
+                                    title="Issue assegnate"
+                                    issues={assignedIssues}
+                                    onViewIssue={onViewIssue}
+                                    onViewAll={() => onNavigation('Issue assegnate')}
+                                />
+                                <IssueSection
+                                    title="Issue segnalate"
+                                    issues={reportedIssues}
+                                    onViewIssue={onViewIssue}
+                                    onViewAll={() => onNavigation('Issue segnalate')}
+                                />
+                            </>
+                        )}
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
