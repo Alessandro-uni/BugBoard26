d4a199d020525a4a1a5b8c530a6bfe3f7e5a6954
il-Drucoder <simo04pet@gmail.com>
2026-06-20 19:47:47 +0200

    Utilizzato passay per gestire la validazione di nuove password

--- Backend/src/main/java/org/swe/bugboard/validation/PasswordValidator.java
+++ Backend/src/main/java/org/swe/bugboard/validation/PasswordValidator.java
@@ -1,15 +1,42 @@
 package org.swe.bugboard.validation;
 
+import org.passay.MessageResolver;
+import org.passay.PasswordData;
+import org.passay.Rule;
+import org.passay.RuleResult;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+import java.util.List;
+
 public class PasswordValidator implements ConstraintValidator<ValidPassword, CharSequence> {
+    private org.passay.PasswordValidator passayValidator;
+
+    @Autowired
+    public void setRules(List<Rule> passwordRules, MessageResolver messageResolver) {
+        this.passayValidator = new org.passay.PasswordValidator(messageResolver, passwordRules);
+    }
+
     @Override
     public boolean isValid(CharSequence rawPassword, ConstraintValidatorContext context) {
         if (rawPassword == null || rawPassword.toString().trim().isEmpty()) {
             return false;
         }
 
-        return rawPassword.toString().matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[#?!@$%^&*-]).{8,}$");
+        RuleResult result = passayValidator.validate(new PasswordData(rawPassword.toString()));
+
+        if (result.isValid()) {
+            return true;
+        }
+
+        context.disableDefaultConstraintViolation();
+        for (String message: passayValidator.getMessages(result)) {
+            context.buildConstraintViolationWithTemplate(message)
+                    .addConstraintViolation();
+        }
+
+        return false;
     }
 }
--- Backend/src/main/java/org/swe/bugboard/validation/ValidPassword.java
+++ Backend/src/main/java/org/swe/bugboard/validation/ValidPassword.java
@@ -9,7 +9,7 @@ import java.lang.annotation.*;
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidPassword {
-    String message() default "Formato password non valido";
+    String message() default "{validation.password.invalid}";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
