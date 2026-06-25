a87d6b002dccaf4ea7339c7de9940100f18c054b
il-Drucoder <simo04pet@gmail.com>
2026-06-25 22:20:12 +0200

    Aggiunto dto per notifiche

---
+++ Backend/src/main/java/org/swe/bugboard/dto/notification/NotificationResponse.java
@@ -0,0 +1,18 @@
+package org.swe.bugboard.dto.notification;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationResponse {
+    private Long id;
+    private String message;
+    private Long idIssue;
+    private LocalDateTime time;
+}
