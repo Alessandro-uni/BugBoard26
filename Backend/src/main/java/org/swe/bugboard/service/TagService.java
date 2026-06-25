e1bfbd756e12febff740f147e02b8079ec750549
il-Drucoder <simo04pet@gmail.com>
2026-06-25 12:54:21 +0200

    Rimosso metodo per cercare tag tramite nome poiché non utilizzato

--- Backend/src/main/java/org/swe/bugboard/service/TagService.java
+++ Backend/src/main/java/org/swe/bugboard/service/TagService.java
@@ -1,11 +1,9 @@
 package org.swe.bugboard.service;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.swe.bugboard.dto.tag.CreateTagRequest;
-import org.swe.bugboard.dto.tag.TagRequest;
 import org.swe.bugboard.dto.tag.TagResponse;
 import org.swe.bugboard.model.Tag;
 import org.swe.bugboard.repository.TagRepository;
@@ -24,14 +22,6 @@ public class TagService {
         Tag savedTag = tagRepository.save(newTag);
 
         return convertModelToResponse(savedTag);
-    }
-
-    @Transactional(readOnly = true)
-    public TagResponse searchTag(TagRequest tagRequest) {
-        Tag tag = tagRepository.findByName(tagRequest.getName())
-                .orElseThrow(() -> new EntityNotFoundException("Tag '" + tagRequest.getName() + "' non trovato"));
-
-        return convertModelToResponse(tag);
     }
 
     @Transactional(readOnly = true)
